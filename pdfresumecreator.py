3eed53db71dd22910b81b4f8eae52464c105bde3
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2022-02-13 02:16:19 +0800

    The program reads the files stored in json data and prints it to pdf

--- pdfresumecreator.py
+++ pdfresumecreator.py
@@ -1,6 +1,13 @@
 # import pdf library and json module
 import json
 from fpdf import FPDF
+pdf = FPDF()
+
+# add a page for pdf
+pdf.add_page()
+
+# set font size and style
+pdf.set_font('Arial', size=10, ) 
 
 # open and read the json file
 data = open('cvdetails.json')
@@ -8,8 +15,12 @@ jsonData = data.read()
 obj = json.loads(jsonData)
 # check if you can access the data 
 for i in obj:
-    print(i)
-# add a page for pdf
-# set font size and style
-# add image: set location and size
+    
 # test if it can print the data in the json file
+
+    pdf.cell(200,5, txt = i , ln=1, align='L')
+
+# add image: set location and size
+
+#save as pdf
+pdf.output('BERMUNDO_JACQUELINE.pdf')
